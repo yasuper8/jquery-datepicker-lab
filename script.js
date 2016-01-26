73a1df852dee9f453bbd435201f4333f8f2d4965
Ben Hulan <benhulan@gmail.com>
2016-01-26 09:56:44 -0800

    removes steps 2 through 6

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-var today = new Date();
-var diffDays = 0;
-var m_names = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec");
-var curr_date = today.getDate();
-var curr_month = today.getMonth();
-var curr_year = today.getFullYear();
+// Before you begin, take a good look at index.html
+// You don't have to make any changes to that file, but you should be familiar with the structure of the file--especially its id's.
 
-$("#todayDate").html(m_names[curr_month] + ". " + curr_date + ", " + curr_year);
-$("#datepicker").datepicker();
-var resultString = "";
+// Our first challenge is to display the current day, month and year in an HTML string, like this:
+// <p>Today is <span id="todayDate">Oct. 6, 2015</span></p>
 
-function daysLeft() {
-	var a = $( "#datepicker" ).datepicker('getDate').getTime();
-	var b = today.getTime();
-	// c = hours per day * minutes per hour * seconds per minute * ms per sec
-	var c = 24*60*60*1000;
+// 1. To begin, we need a variable to reference the current time when a user loads the page.
+// Create a variable "today" as an instance of the Date object.
 
-	// here we use the native Math object to solve a problem:
-	var diffDays = Math.round((a - b)/c);
+_____
 
-	if( $('input').val() ){
-		if (diffDays < 0 ) {
-			resultString = "<p>Please pick a date in the future.</p>";
-		} else if (diffDays === 1){
-			resultString = ("<p>You only have ONE day left until " + $('input').val() + "!</p>");
-		} else {
-			resultString = ("<p>You have " + diffDays + " days left until " + $('input').val() + "!</p>");
-		}
-	} else {
-		if (diffDays < 0 ) {
-			resultString = "<p>Please pick a date in the future.</p>";
-		} else if (diffDays === 1){
-			resultString = ("<p>You only have ONE day left!</p>");
-		} else {
-			resultString = ("<p>You have " + diffDays + " days left!");
-		}
-	}
-	// return diffDays;
-};
+// You can read all about the Date object here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
-$("#datepicker").on("change", function(){
-	daysLeft();
-	$("#result").html(resultString);
-});
 
-$("button").click(function(){
-	$("#result").addClass("showResult");
-});
+// 2. Next we need the values for the current day, month and year.
+// The Date object gives us a ton of methods to get the data we want.
+// Create variables for the current date (day of the month, 1-31), current month (0-11) and current year (2015).
+// Assign values for each by calling the following methods on the today variable:
+// Date.getDate();
+// Date.getMonth();
+// Date.getFullYear();
 
+_____
+_____
+_____
+
+// 3. As we saw in class, Date.getMonth(); returns a number. We want a three-letter abbreviation for the month.
+// Maybe we can get that from the Date object, but let's just make our own array and use the data we already have.
+// Create a month names array using the new keyword and the Array() prototype object.
+// Populate the array with three-letter string abbreviations for each month.
+
+_____
+
+// 4. Now we have everything we need to create our HTML string. Use jQuery's .html() method.
+
+$("#todayDate").html(____);
